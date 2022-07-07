ba89a73c786b0bc8910eea4f458d500d1a27d4a7
Felipe Carvalho <darkdeusloki@gmail.com>
2022-07-07 11:24:54 -0300

    # Arrumando o rosto
    ** Configurando tela cheia no mini dell

--- Trabalho/Assistente/rosto/rosto.py
+++ Trabalho/Assistente/rosto/rosto.py
@@ -1,15 +1,16 @@
 import pyglet
 
-
 animation = pyglet.image.load_animation('falando.gif')
-animation2 = pyglet.image.load_animation('dormindo.gif')
+animation2 = pyglet.image.load_animation('piscando.gif')
 animation3 = pyglet.image.load_animation('erro.gif')
 animSprite = pyglet.sprite.Sprite(animation)
+animSprite2 = pyglet.sprite.Sprite(animation2)
+animSprite3 = pyglet.sprite.Sprite(animation3)
 
 w = animSprite.width
 h = animSprite.height
 
-window = pyglet.window.Window(width=w, height=h)
+window = pyglet.window.Window(width = w, height = h, fullscreen = True)
 
 r, g, b, alpha = 0.5, 0.5, 0.8, 0.5
 
